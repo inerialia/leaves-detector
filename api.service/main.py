a77970375d9826593133dd322eb386b20b88114a
Inerialia <annashar@gmail.com>
2021-04-23 20:10:16 +0200

    added port to main.py

--- api.service/main.py
+++ api.service/main.py
@@ -47,7 +47,7 @@ origins = [
     "http://localhost:4200",
     "http://ui.service.localhost",
     "http://192.168.12.34",
-    "http://192.168.1.14"
+    "http://192.168.1.14:8080"
 ]
 
 app.add_middleware(
