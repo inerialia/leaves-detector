9d39d43ea62b81a540628f2dde486e7a00d3dded
Inerialia <annashar@gmail.com>
2021-04-23 20:02:39 +0200

    added 192.168.1.14 to main.py

--- api.service/main.py
+++ api.service/main.py
@@ -46,7 +46,8 @@ app = FastAPI()
 origins = [
     "http://localhost:4200",
     "http://ui.service.localhost",
-    "http://192.168.12.34"
+    "http://192.168.12.34",
+    "http://192.168.1.14"
 ]
 
 app.add_middleware(
